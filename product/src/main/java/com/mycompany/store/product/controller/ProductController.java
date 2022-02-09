04c547f91a2acf0f5d486b85cf5890b957fb3466
Antony Itteera <antonyItteera@gmail.com>
2022-02-09 01:43:15 +0000

    dasdasd

--- product/src/main/java/com/mycompany/store/product/controller/ProductController.java
+++ product/src/main/java/com/mycompany/store/product/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.mycompany.store.product.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.mycompany.store.product.dao.ProductRepository;
 import com.mycompany.store.product.entity.Product;
 
+@CrossOrigin("*")
 @RestController
 public class ProductController {
 	@Autowired
